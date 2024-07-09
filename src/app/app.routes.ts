dc00e77fb19fb26d54258af4e09ade103835785a
georgedaniel.cristea <georgedaniel.cristea@bcr.ro>
2024-07-09 15:55:46 +0300

    added routes for routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import {AppRoutes} from "./shared/routes/routes.enum";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: AppRoutes.PROFILE,
+    pathMatch: 'full'
+  },
+  {
+    path: AppRoutes.PROFILE,
+    loadComponent: () => import('./pages/profile/profile.component').then((component) => component.ProfileComponent)
+  },
+  {
+    path: AppRoutes.REGISTER,
+    loadComponent: () => import('./pages/register/register.component').then((component) => component.RegisterComponent)
+  },
+  {
+    path: AppRoutes.LOGIN,
+    loadComponent: () => import('./pages/login/login.component').then((component) => component.LoginComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/not-found/not-found.component').then((component) => component.NotFoundComponent)
+  }
+];
