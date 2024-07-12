8d71cbf742e49e28ea7382fad11e06b766437557
georgedaniel.cristea <georgedaniel.cristea@bcr.ro>
2024-07-12 13:19:45 +0300

    added button component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {InputComponent} from "./components/input/input.component";
+import {ButtonComponent} from "./components/button/button.component";
 
 @NgModule({
   imports: [
     CommonModule,
     NgOptimizedImage,
-    InputComponent
+    InputComponent,
+    ButtonComponent
   ],
   exports: [
     CommonModule,
     NgOptimizedImage,
-    InputComponent
+    InputComponent,
+    ButtonComponent
   ]
 })
 export class SharedModule { }
